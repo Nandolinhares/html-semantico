0866b4691e5eb8fdb3fb48817ffa58944550dc07
nandolinhares <fernandolinhares1920@gmail.com>
2022-05-13 16:37:36 -0300

    impleented an example of use

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <section>
+      <header className='header-pag'>
+        <h1 title="Logo Finance">Stone Co. Finance</h1>
+        <nav>
+          <h2>Menu</h2>
+          <ul>
+            <li title="Home link"><a href="https://www.google.com" target="_blank" rel="noreferrer">Home</a></li>
+            <li title="A empresa link" ><a href='https://www.google.com' target="_blank" rel="noreferrer">A empresa</a></li>
+          </ul>
+        </nav>
       </header>
-    </div>
+      <main>
+        {Array.from({ length: 8 }).map((_, index) => (
+          <article key={index}>
+            <header className='header-article'>
+              <h2>Card {index}</h2>
+              <p>Algum conteúdo a falar</p>
+              <time dateTime='2022-11-03' >03/11/2022</time>
+            </header>
+            <section>
+              <h4>Conteúdo do body do card 1</h4>
+              <p>Cheio de conteúdo</p>
+            </section>
+            <footer>
+              <address>
+                <a href='mailto:testando@test.com'>testando@teste.com</a>
+              </address>
+            </footer>
+          </article>
+        ))}
+      </main>
+      <footer className='footer-pag'>
+        <h3>Todos os direitos reservados a Stone Co</h3>
+        <address>
+          <a href='mailto:testando@test.com'>testando@teste.com</a>
+        </address>
+      </footer>
+    </section>
   );
 }
 
